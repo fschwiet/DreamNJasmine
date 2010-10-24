0c7ceb5a306ff54cbd4011b63898482e8cc05b2f
fschwiet <fschwiet@gmail.com>
2010-10-24 12:46:40 -0700

    what I want

---
+++ NJasmine/NJasmineFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NJasmine
+{
+    public abstract class NJasmineFixture
+    {
+        public abstract void BuildTests(Action action);
+
+        public void describe(string description, Action action)
+        {
+        }
+
+        public void beforeEach(Action action)
+        {
+        }
+
+        public void forEach<T>(Func<IEnumerable<T>> testCases, Action<T> action)
+        {
+        }
+
+        public void afterEach(Action action)
+        {
+        }
+
+        public void it(string description, Action action)
+        {
+        }
+    }
+}
