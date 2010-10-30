71584a9064a36122e572e6588ba166a2089985fa
fschwiet <fschwiet@gmail.com>
2010-10-30 13:32:45 -0700

    added failing tests for NUnit fixture support

--- NJasmine.Tests/Core/build_and_run_suite_importing_NUnit_fixture.cs
+++ NJasmine.Tests/Core/build_and_run_suite_importing_NUnit_fixture.cs
@@ -6,111 +6,81 @@ using NUnit.Framework;
 
 namespace NJasmineTests.Core
 {
-    public class build_and_run_suite_importing_NUnit_fixture
+    [Explicit, TestFixture]
+    public class some_Nunit_fixture_a
     {
-        [Explicit, TestFixture]
-        public class some_Nunit_fixture_a
+        [TestFixtureSetUp]
+        public void FixtureSetup()
         {
-            [TestFixtureSetUp]
-            public void FixtureSetup()
-            {
-                imports_NUnit_fixture.Trace("FixtureSetup " + this.GetType());
-            }
-
-            [TestFixtureTearDown]
-            public void FixtureTearDown()
-            {
-                imports_NUnit_fixture.Trace("FixtureTearDown" + this.GetType());
-            }
-
-            [SetUp]
-            public void SetUp()
-            {
-                imports_NUnit_fixture.Trace("SetUp" + this.GetType());
-            }
-
-            [TearDown]
-            public void TearDown()
-            {
-                imports_NUnit_fixture.Trace("TearDown" + this.GetType());
-            }
+            imports_NUnit_fixture.Trace("FixtureSetup " + this.GetType());
         }
 
-        [Explicit, TestFixture]
-        public class some_Nunit_fixture_b : some_Nunit_fixture_a
+        [TestFixtureTearDown]
+        public void FixtureTearDown()
         {
+            imports_NUnit_fixture.Trace("FixtureTearDown " + this.GetType());
         }
 
-        public class imports_NUnit_fixture : ObservableNJasmineFixture
+        [SetUp]
+        public void SetUp()
         {
-            public override void Tests()
-            {
-                Trace("test started, before first include of a");
-                var firstA = importNUnit<some_Nunit_fixture_a>();
-                Trace("after first include of a");
+            imports_NUnit_fixture.Trace("SetUp " + this.GetType());
+        }
 
-                describe("a block", () =>
-                {
-                    Trace("first describe, before include of b");
-                    var firstB = importNUnit<some_Nunit_fixture_b>();
-                    Trace("after include of b");
-
-                    it("check fixtures", () =>
-                    {
-                        Trace("first test");
-                        //expect(firstA.Observations).to.Equal(new List<string>()
-                        //{
-                        //    "FixtureSetup",
-                        //    "SetUp"
-                        //});
-
-                        //expect(firstB.Observations).to.Equal(new List<string>()
-                        //{
-                        //    "FixtureSetup",
-                        //    "SetUp"
-                        //});
-                    });
-
-                    describe("a sub block", () =>
-                    {
-                        Trace("before second a");
-                        var secondA = importNUnit<some_Nunit_fixture_a>();
-                        Trace("after second a");
-
-                        it("check fixtures again", () =>
-                        {
-                            Trace("second test test");
-
-                            //expect(firstA.Observations).to.Equal(new List<string>()
-                            //{
-                            //    "FixtureSetup",
-                            //    "SetUp",
-                            //    "TearDown",
-                            //    "SetUp"
-                            //});
-
-                            //expect(firstB.Observations).to.Equal(new List<string>()
-                            //{
-                            //    "FixtureSetup",
-                            //    "SetUp",
-                            //    "TearDown",
-                            //    "SetUp"
-                            //});
-
-                            //expect(secondA.Observations).to.Equal(new List<string>()
-                            //{
-                            //    "FixtureSetup",
-                            //    "SetUp"
-                            //});
-                        });
-                    });
-                });
-            }
-
-            public static void Trace(string value)
-            {
-                Console.WriteLine("<<{{" + value + "}}>>");
-            }
+        [TearDown]
+        public void TearDown()
+        {
+            imports_NUnit_fixture.Trace("TearDown " + this.GetType());
         }
     }
+
+    [Explicit, TestFixture]
+    public class some_Nunit_fixture_b : some_Nunit_fixture_a
+    {
+    }
+
+    [Explicit, TestFixture]
+    public class some_Nunit_fixture_c : some_Nunit_fixture_a
+    {
+    }
+
+    public class imports_NUnit_fixture : ObservableNJasmineFixture
+    {
+        public override void Tests()
+        {
+            Trace("test started, before include of a");
+            var firstA = importNUnit<some_Nunit_fixture_a>();
+            Trace("after include of a");
+
+            describe("a block", () =>
+            {
+                Trace("first describe, before include of b");
+                var firstB = importNUnit<some_Nunit_fixture_b>();
+                Trace("after include of b");
+
+                it("check fixtures", () =>
+                {
+                    Trace("first test");
+                });
+
+                describe("a sub block", () =>
+                {
+                    Trace("before include of c");
+                    var secondA = importNUnit<some_Nunit_fixture_c>();
+                    Trace("after include of c");
+
+                    it("check fixtures again", () =>
+                    {
+                        Trace("second test test");
+                    });
+                });
+            });
+        }
+
+        public static void Trace(string value)
+        {
+            Console.WriteLine("<<{{" + value + "}}>>");
+        }
+    }
+
 }
--- default.ps1
+++ default.ps1
@@ -16,6 +16,23 @@ task Build {
 	exec { & $msbuild $sln /property:Configuration=$msbuild_Configuration }
 }
 
+
+function RunNUnit {
+
+    param ($dll, $run = "")
+
+    $command = @( $nunit_path, $testDll)
+    
+    if ($run) {
+        $command += /run:$run
+    }
+
+    $command = [String]::Join(" ", $command)
+
+    exec { invoke-expression $command }
+    
+}
+
 task TestDeploy -depends Build {
 
 	if (test-path $testDeployTarget) {
@@ -25,7 +42,59 @@ task TestDeploy -depends Build {
 	cp $deploySource\* $testDeployTarget -recurse
 }
 
+task TestFixture_imports_NUnit_fixture {
+
+    $tests = @(
+        @{ 
+            test = "NJasmineTests.Core.imports_NUnit_fixture";
+            expected = @(
+                "test started, before first include of a",
+                "after first include of a",
+                "first describe, before include of b",
+                "after include of b",
+                "before second a",
+                "after second a",
+                "FixtureSetup some_Nunit_fixture_a",
+                "FixtureSetup some_Nunit_fixture_b",
+                "FixtureSetup some_Nunit_fixture_a",
+                "test started, before first include of a",
+                "Setup some_Nunit_fixture_a",
+                "after first include of a",
+                "first describe, before include of b",
+                "Setup some_Nunit_fixture_b",
+                "after include of b",
+                "before second a",
+                "Setup some_Nunit_fixture_a",
+                "after second a",
+                "second test test",
+                "test started, before first include of a",
+                "after first include of a",
+                "first describe, before include of b",
+                "after include of b",
+                "first test")
+        }
+    )
+
+    $tests | % { 
+
+        $test = $_.test;
+        $expected = $_.expected;
+	    $testoutput = exec { & $nunit_path $testDll /run:NJasmineTests.Core.imports_NUnit_fixture }
+        $actual = switch -r ($testoutput) { "<<{{(.*)}}>>" { $matches[1] } }
+        $comparison = compare-object $expected $actual
+        if ($comparison) {
+            $global:expected = $expected;
+            $global:actual = $actual;
+            write-error "Unexpected results for `"$test`".  Expected written to `$global:expected, actual written to `$global:actual."
+        }
+    }
+}
+
 task Test -depends Build {
 
-	exec { & $nunit_path $testDll }
+	#exec { & $testDll }
+
+    RunNUnit $testDll
+
 }
+
