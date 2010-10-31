30f4885cc5ef3842386ac48602ef51ac69284351
fschwiet <fschwiet@gmail.com>
2010-10-30 17:20:23 -0700

    retrying NUnit fixture support

---
+++ NJasmine.Tests/ImportNUnitFixture/MultifixtureTests.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NJasmine;
+using NJasmine.Core;
+using NJasmine.ImportNUnitFixture;
+using NUnit.Framework;
+
+namespace NJasmineTests.ImportNUnitFixture
+{
+    public class MultifixtureTests : NJasmineFixture
+    {
+        public override void Tests()
+        {
+
+describe("Multifixture", delegate
+{
+    var sut = new Multifixture();
+
+    describe("a collection of fixtures", delegate
+    {
+        var position = new TestPosition();
+        var type = typeof(SomeFixtureTypeA);
+
+        it("tracks fixture types by test position", delegate
+        {
+            sut.AddFixture(position, type);
+
+            expect(sut.GetFixture(position)).to.Equal(type);
+        });
+
+        it("can only add one fixture per position", delegate
+        {
+            sut.AddFixture(position, type);
+
+            try
+            {
+                sut.AddFixture(position, type);
+            }
+            catch (InvalidOperationException e)
+            {
+                return;
+            }
+            
+            Assert.Fail("exception expected");
+        });
+
+        it("can retrive the fixture instance at a position", delegate
+        {
+            sut.AddFixture(position, type);
+
+            var instance1 = sut.GetInstance(position);
+            var instance2 = sut.GetInstance(position);
+
+            expect(instance1).to.Be.OfType(type);
+            expect(instance1).to.Be.SameAs(instance2);
+        });
+    });
+
+    describe("can run one-time fixture setup and teardown", delegate
+    {
+        beforeEach(delegate
+        {
+            Observed = new StringBuilder();
+        });
+
+        it("for an empty collection", delegate
+        {
+            sut.DoOnetimeSetUp();
+            expect(Observed.ToString()).to.Equal("");
+
+            Observed = new StringBuilder();
+
+            sut.DoOnetimeTearDown();
+            expect(Observed.ToString()).to.Equal("");
+        });
+
+        it("for one", delegate
+        {
+            sut.AddFixture(new TestPosition(0), typeof(SomeFixtureTypeA));
+            sut.DoOnetimeSetUp();
+
+            expect(Observed.ToString()).to.Equal("A.TestFixtureSetUp ");
+
+            Observed = new StringBuilder();
+
+            sut.DoOnetimeTearDown();
+            expect(Observed.ToString()).to.Equal("A.TestFixtureTearDown ");
+        });
+
+        it("and more", delegate
+        {
+            sut.AddFixture(new TestPosition(0), typeof(SomeFixtureTypeA));
+            sut.AddFixture(new TestPosition(1), typeof(SomeFixtureTypeB));
+            sut.AddFixture(new TestPosition(2), typeof(SomeFixtureTypeC));
+            sut.DoOnetimeSetUp();
+
+            expect(Observed.ToString()).to.Equal("A.TestFixtureSetUp B.TestFixtureSetUp C.TestFixtureSetUp ");
+
+            Observed = new StringBuilder();
+
+            sut.DoOnetimeTearDown();
+            expect(Observed.ToString()).to.Equal("C.TestFixtureTearDown B.TestFixtureTearDown A.TestFixtureTearDown ");
+        });
+    });
+});
+
+        }
+
+        class SomeFixtureTypeA
+        {
+            public char ShortName { get { return this.GetType().ToString().Last(); }}
+
+            [TestFixtureSetUp]
+            public void TestFixtureSetUp() { Observed.Append(ShortName + ".TestFixtureSetUp "); }
+
+            [TestFixtureTearDown]
+            public void TestFixtureTearDown() { Observed.Append(ShortName + ".TestFixtureTearDown "); }
+
+            [SetUp]
+            public void SetUp() { Observed.Append(ShortName + ".SetUp "); }
+
+            [TearDown]
+            public void TearDown() { Observed.Append(ShortName + ".TearDown "); }
+        }
+
+        class SomeFixtureTypeB : SomeFixtureTypeA { }
+
+        class SomeFixtureTypeC : SomeFixtureTypeA { }
+
+        static StringBuilder Observed;
+    }
+}
---
+++ NJasmine/ImportNUnitFixture/Multifixture.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NJasmine.Core;
+using NUnit.Core;
+
+namespace NJasmine.ImportNUnitFixture
+{
+    public class Multifixture
+    {
+        List<TestPosition> _positions = new List<TestPosition>();  // storing position keys separately by order of existence
+        Dictionary<TestPosition, Type> _fixtures = new Dictionary<TestPosition, Type>();
+        Dictionary<TestPosition, object> _instances = new Dictionary<TestPosition, object>();
+        
+        public void DoOnetimeSetUp()
+        {
+            foreach(var instance in _positions.Select(p => GetInstance(p)))
+            {
+                var methods = NUnit.Core.Reflect.GetMethodsWithAttribute(instance.GetType(),
+                                                                         NUnitFramework.FixtureSetUpAttribute, true);
+
+                foreach(var method in methods)
+                {
+                    method.Invoke(instance, EmptyObjectArray);
+                }
+            }
+        }
+
+        public void DoOnetimeTearDown()
+        {
+            foreach (var instance in _positions.Select(p => GetInstance(p)).Reverse())
+            {
+                var methods = NUnit.Core.Reflect.GetMethodsWithAttribute(instance.GetType(),
+                                                                         NUnitFramework.FixtureTearDownAttribute, true);
+
+                foreach (var method in methods)
+                {
+                    method.Invoke(instance, EmptyObjectArray);
+                }
+            }
+        }
+
+        public void AddFixture(TestPosition position, Type type)
+        {
+            if (_fixtures.ContainsKey(position))
+            {
+                throw new InvalidOperationException();
+            }
+
+            _positions.Add(position);
+            _fixtures[position] = type;
+        }
+
+        public Type GetFixture(TestPosition position)
+        {
+            return _fixtures[position];
+        }
+
+        public object GetInstance(TestPosition position)
+        {
+            object instance = null; 
+
+            if (!_instances.TryGetValue(position, out instance))
+            {
+                var type = GetFixture(position);
+
+                instance = type.GetConstructor(new Type[0]).Invoke(EmptyObjectArray);
+                _instances[position] = instance;
+            }
+
+            return instance;
+        }
+
+        readonly static object[] EmptyObjectArray = new object[0];
+    }
+}
