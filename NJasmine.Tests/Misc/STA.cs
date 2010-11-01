d8086f1ab78002d2e95a1e7ec20d46ebdc9115fe
fschwiet <fschwiet@gmail.com>
2010-11-01 01:33:52 -0700

    added test to verify RequireSTA works

---
+++ NJasmine.Tests/Misc/STA.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using NJasmine;
+using NUnit.Framework;
+
+namespace NJasmineTests.Misc
+{
+    [RequiresSTA]
+    public class STA : NJasmineFixture
+    {
+        [STAThread]
+        public override void Tests()
+        {
+            it("is STA", delegate
+            {
+                expect(System.Threading.Thread.CurrentThread.GetApartmentState()).to.Equal(ApartmentState.STA);
+            });
+        }
+    }
+}
