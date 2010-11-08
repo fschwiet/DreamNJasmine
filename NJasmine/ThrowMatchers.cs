9938b37d4895db70590d8f0bdb8e880c67328f19
fschwiet <fschwiet@gmail.com>
2010-11-07 22:40:17 -0800

    adding Should.Fluent with custom matchers support, and trying to add some custom matchers.

---
+++ NJasmine/ThrowMatchers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Should.Fluent.Model;
+using Should.Fluent.Extensions;
+
+namespace NJasmine
+{
+    public static class ThrowMatchers
+    {
+        public static void Throw<TException>(this Should<Action, Be<Action>> should) where TException : Exception
+        {
+            Exception e = null;
+
+            try
+            {
+                should.Actual()();
+            }
+            catch (Exception thrown)
+            {
+                e = thrown;
+            }
+
+            if (should.ShouldNegate())
+            {
+                if (typeof(TException).IsInstanceOfType(e))
+                {
+                    should.Assert().Fail("Expected not to see exception of type {0}, saw exception of type {1}.", typeof(TException).Name, e.GetType().Name);
+                }
+            }
+            else
+            {
+                if (e == null)
+                {
+                    should.Assert().Fail("Expected exception of type {0}, no exception was thrown.", typeof(TException).Name);
+                }
+                else if (!typeof(TException).IsInstanceOfType(e))
+                {
+                    should.Assert().Fail("Expected exception of type {0}, saw exception of type {1}.", typeof(TException).Name, e.GetType().Name);
+                }
+            }
+        }
+    }
+}
