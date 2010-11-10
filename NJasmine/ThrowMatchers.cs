099bc6ae55fcd3bda4106800e059cf32ae65a615
fschwiet <fschwiet@gmail.com>
2010-11-09 22:56:55 -0800

    updated throw matchers to use latest Should.Fluent extensibility model

--- NJasmine/ThrowMatchers.cs
+++ NJasmine/ThrowMatchers.cs
@@ -3,43 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Should.Fluent.Model;
-using Should.Fluent.Extensions;
 
 namespace NJasmine
 {
     public static class ThrowMatchers
     {
-        public static void Throw<TException>(this Should<Action, Be<Action>> should) where TException : Exception
+        public static void Throw<TException>(this IShould<Action> should) where TException : Exception
         {
-            Exception e = null;
+            should.Apply((actual, assert) =>
+                {
+                    Exception e = null;
 
-            try
-            {
-                should.Actual()();
-            }
-            catch (Exception thrown)
-            {
-                e = thrown;
-            }
+                    try
+                    {
+                        actual();
+                    }
+                    catch (Exception thrown)
+                    {
+                        e = thrown;
+                    }
 
-            if (should.ShouldNegate())
-            {
-                if (typeof(TException).IsInstanceOfType(e))
+                    if (e == null)
+                    {
+                        assert.Fail("Expected exception of type {0}, no exception was thrown.", typeof(TException).Name);
+                    }
+                    else if (!typeof(TException).IsInstanceOfType(e))
+                    {
+                        assert.Fail("Expected exception of type {0}, saw exception of type {1}.", typeof(TException).Name, e.GetType().Name);
+                    }
+
+                    return actual;
+                },
+                (actual, assert) =>
                 {
-                    should.Assert().Fail("Expected not to see exception of type {0}, saw exception of type {1}.", typeof(TException).Name, e.GetType().Name);
-                }
-            }
-            else
-            {
-                if (e == null)
-                {
-                    should.Assert().Fail("Expected exception of type {0}, no exception was thrown.", typeof(TException).Name);
-                }
-                else if (!typeof(TException).IsInstanceOfType(e))
-                {
-                    should.Assert().Fail("Expected exception of type {0}, saw exception of type {1}.", typeof(TException).Name, e.GetType().Name);
-                }
-            }
+                    Exception e = null;
+
+                    try
+                    {
+                        actual();
+                    }
+                    catch (Exception thrown)
+                    {
+                        e = thrown;
+                    }
+
+                    if (typeof(TException).IsInstanceOfType(e))
+                    {
+                        assert.Fail("Expected not to see exception of type {0}, saw exception of type {1}.", typeof(TException).Name, e.GetType().Name);
+                    }
+
+                    return actual;
+                });
         }
     }
 }
