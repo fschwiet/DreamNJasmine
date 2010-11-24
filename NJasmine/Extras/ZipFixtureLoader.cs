9ea918cf04cd51a1e93541f11075300082118fdb
fschwiet <fschwiet@gmail.com>
2010-11-23 18:30:56 -0800

    zip fixture extraction is a bit more robust

--- NJasmine/Extras/ZipFixtureLoader.cs
+++ NJasmine/Extras/ZipFixtureLoader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Threading;
 
 namespace NJasmine.Extras
 {
@@ -17,16 +18,14 @@ namespace NJasmine.Extras
         {
             string tempPath = tempPath = Path.Combine(Path.GetTempPath(), tempName);
 
-            if (Directory.Exists(tempPath))
-            {
-                Directory.Delete(tempPath, true);
-            }
+            DeleteDirectory(tempPath);
 
             ProcessStartInfo info = new ProcessStartInfo(Get7Zip(),
                                                          String.Format("x -o{1} \"{2}\"", Get7Zip(), tempPath,
                                                                        sourceZipFilepath));
             info.WorkingDirectory = Path.GetTempPath();
             info.UseShellExecute = false;
+            info.CreateNoWindow = true;
             info.RedirectStandardOutput = true;
             info.RedirectStandardError = true;
 
@@ -60,6 +59,43 @@ namespace NJasmine.Extras
         private static string Get7Zip()
         {
             return GetAndCheckPathOfBinDeployedFile("7za.exe");
+        }
+
+        public static void DeleteDirectory(string directory)
+        {
+            const int retries = 10;
+            for (int i = 0; i < retries; i++)
+            {
+                try
+                {
+                    if (Directory.Exists(directory) == false)
+                        return;
+
+                    try
+                    {
+                        File.SetAttributes(directory, FileAttributes.Normal);
+                    }
+                    catch (IOException)
+                    {
+                    }
+                    Directory.Delete(directory, true);
+                    return;
+                }
+                catch (IOException)
+                {
+                    foreach (var childDir in Directory.GetDirectories(directory))
+                    {
+                        try
+                        {
+                            File.SetAttributes(childDir, FileAttributes.Normal);
+                        }
+                        catch (IOException)
+                        {
+                        }
+                    }
+                    Thread.Sleep(100);
+                }
+            }
         }
     }
 }
