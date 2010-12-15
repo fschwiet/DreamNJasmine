45d1496faf16cfd022439597028641d47335bc37
fschwiet <fschwiet@gmail.com>
2010-12-15 13:46:01 -0800

    fixing break

--- NJasmine.Tests/PassingFixtures/imports_NUnit_fixture.cs
+++ NJasmine.Tests/PassingFixtures/imports_NUnit_fixture.cs
@@ -42,7 +42,6 @@ namespace NJasmineTests.PassingFixtures
     }
 
     [RunExternal(true, ExpectedTraceSequence = @"
-lalala 1235598425
 FixtureSetup some_Nunit_fixture_a
 FixtureSetup some_Nunit_fixture_b
 test started, before include of a
