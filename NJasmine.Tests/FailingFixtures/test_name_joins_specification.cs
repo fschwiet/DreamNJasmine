d70182a32925afc3591a8f0e2e6e34198f015820
fschwiet <fschwiet@gmail.com>
2010-12-18 13:31:44 -0800

    adding test for names

---
+++ NJasmine.Tests/FailingFixtures/test_name_joins_specification.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NJasmine;
+using NUnit.Framework;
+
+namespace NJasmineTests.FailingFixtures
+{
+    [Explicit]
+    //[RunExternal(false, ExpectedStrings = new string[] { @"in progress......"})]
+    public class test_name_joins_specification : NJasmineFixture
+    {
+        public override void Tests()
+        {
+            it("simple test");
+            
+            describe("simple describe", delegate
+            {
+                it("simple test");
+            });
+
+            describe("describe with setup", delegate
+            {
+                //arrange("simple arrange");
+
+                it("simple test");
+            });
+
+        }
+    }
+}
