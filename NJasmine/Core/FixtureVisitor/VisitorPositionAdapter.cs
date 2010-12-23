1cfff860173b0b0775b2670aa3fd320ffa6f6441
fschwiet <fschwiet@gmail.com>
2010-12-22 20:19:17 -0800

    VisitorPositionAdapter extended to support more re-entrancy and still track a valid position

--- NJasmine/Core/FixtureVisitor/VisitorPositionAdapter.cs
+++ NJasmine/Core/FixtureVisitor/VisitorPositionAdapter.cs
@@ -1,75 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace NJasmine.Core.FixtureVisitor
 {
     public class VisitorPositionAdapter : INJasmineFixtureVisitor
     {
         readonly INJasmineFixturePositionVisitor _visitor;
-        protected TestPosition _nextPosition;
+        protected TestPosition _position;
 
         public VisitorPositionAdapter(INJasmineFixturePositionVisitor visitor)
         {
             _visitor = visitor;
-            _nextPosition = new TestPosition(0);
+            _position = new TestPosition(0);
         }
 
         public VisitorPositionAdapter(TestPosition position, INJasmineFixturePositionVisitor visitor)
         {
             _visitor = visitor;
-            _nextPosition = position;
+            _position = position;
         }
 
         private void DoThenAdvancePosition(Action action)
         {
-            var nextSibling = _nextPosition.GetNextSiblingPosition();
+            var nextSibling = _position.GetNextSiblingPosition();
 
             action();
 
-            _nextPosition = nextSibling;
+            _position = nextSibling;
+        }
+
+        private Action WrapActionToRunAtFirstchildPosition(Action action)
+        {
+            if (action == null)
+                return null;
+
+            var firstChildPosition = _position.GetFirstChildPosition();
+
+            return delegate
+            {
+                _position = firstChildPosition;
+
+                action();
+            };
+        }
+
+        private IEnumerable<Func<T>> WrapFunctionToRunAtChildPosition<T>(IEnumerable<Func<T>> funcs)
+        {
+            List<Func<T>> result = new List<Func<T>>();
+            var nextChildPosition = _position.GetFirstChildPosition();
+
+            foreach(var func in funcs)
+            {
+                var thisPosition = nextChildPosition;
+                var thisFunc = func;
+
+                result.Add(delegate
+                {
+                    _position = thisPosition.GetFirstChildPosition();
+                    return thisFunc();
+                });
+
+                nextChildPosition = nextChildPosition.GetNextSiblingPosition();
+            }
+
+            return result;
         }
 
         public void visitDescribe(string description, Action action)
         {
-            DoThenAdvancePosition(delegate
-            {
-                if (action == null)
-                {
-                    _visitor.visitDescribe(description, null, _nextPosition);
-                }
-                else
-                {
-                    var firstChildPosition = _nextPosition.GetFirstChildPosition();
-
-                    _visitor.visitDescribe(description, delegate()
-                    {
-                        _nextPosition = firstChildPosition;
-                        action();
-                    }, _nextPosition);
-                }
-            });
+            DoThenAdvancePosition(() => 
+                _visitor.visitDescribe(description, WrapActionToRunAtFirstchildPosition(action), _position));
         }
 
         public void visitBeforeEach(Action action)
         {
-            DoThenAdvancePosition(() => _visitor.visitBeforeEach(action, _nextPosition));
+            DoThenAdvancePosition(() => 
+                _visitor.visitBeforeEach(WrapActionToRunAtFirstchildPosition(action), _position));
         }
 
         public void visitAfterEach(Action action)
         {
-            DoThenAdvancePosition(() => _visitor.visitAfterEach(action, _nextPosition));
+            DoThenAdvancePosition(() => 
+                _visitor.visitAfterEach(WrapActionToRunAtFirstchildPosition(action), _position));
         }
 
         public void visitIt(string description, Action action)
         {
-            DoThenAdvancePosition(() => _visitor.visitIt(description, action, _nextPosition));
+            DoThenAdvancePosition(() => 
+                _visitor.visitIt(description, WrapActionToRunAtFirstchildPosition(action), _position));
         }
 
         public TFixture visitImportNUnit<TFixture>() where TFixture: class, new()
         {
             TFixture result = null;
 
-            DoThenAdvancePosition(() => result = _visitor.visitImportNUnit<TFixture>(_nextPosition));
+            DoThenAdvancePosition(() => 
+                result = _visitor.visitImportNUnit<TFixture>(_position));
 
             return result;
         }
@@ -78,7 +105,10 @@ namespace NJasmine.Core.FixtureVisitor
         {
             TArranged result = default(TArranged);
 
-            DoThenAdvancePosition(() => result = _visitor.visitArrange(description, factories, _nextPosition));
+            factories = WrapFunctionToRunAtChildPosition(factories);
+
+            DoThenAdvancePosition(() => result = 
+                _visitor.visitArrange(description, factories, _position));
 
             return result;
         }
