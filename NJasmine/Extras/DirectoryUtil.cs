0f44b6c3bd924135b7fdc7162a2eea4eaa012350
fschwiet <fschwiet@gmail.com>
2010-12-29 14:01:03 -0800

    changed directory delete function

---
+++ NJasmine/Extras/DirectoryUtil.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace NJasmine.Extras
+{
+    public static class DirectoryUtil
+    {
+        public static void DeleteDirectory(string directory)
+        {
+            const int retries = 15;
+            for (int i = 0; i < retries; i++)
+            {
+                try
+                {
+                    if (Directory.Exists(directory) == false)
+                        return;
+
+                    File.SetAttributes(directory, FileAttributes.Normal);
+                    Directory.Delete(directory, true);
+                    return;
+                }
+                catch (IOException)
+                {
+                    foreach (var childDir in Directory.GetDirectories(directory))
+                    {
+                        try
+                        {
+                            File.SetAttributes(childDir, FileAttributes.Normal);
+                        }
+                        catch(IOException)
+                        {
+                        }
+                    }
+
+                    Thread.Sleep(100);
+                }
+            }
+
+            if (Directory.Exists(directory))
+                throw new Exception("Could not delete directory " + directory);
+        }
+    }
+}
--- NJasmine/Extras/ZipFixtureLoader.cs
+++ NJasmine/Extras/ZipFixtureLoader.cs
@@ -19,7 +19,7 @@ namespace NJasmine.Extras
         {
             string tempPath = Path.Combine(Path.GetTempPath(), tempName);
 
-            DeleteDirectory(tempPath);
+            DirectoryUtil.DeleteDirectory(tempPath);
             Directory.CreateDirectory(tempPath);
             
             ZipInputStream s = new ZipInputStream(File.OpenRead(sourceZipFilepath));
@@ -107,43 +107,6 @@ namespace NJasmine.Extras
         private static string Get7Zip()
         {
             return GetAndCheckPathOfBinDeployedFile("7za.exe");
-        }
-
-        public static void DeleteDirectory(string directory)
-        {
-            const int retries = 10;
-            for (int i = 0; i < retries; i++)
-            {
-                try
-                {
-                    if (Directory.Exists(directory) == false)
-                        return;
-
-                    try
-                    {
-                        File.SetAttributes(directory, FileAttributes.Normal);
-                    }
-                    catch (IOException)
-                    {
-                    }
-                    Directory.Delete(directory, true);
-                    return;
-                }
-                catch (IOException)
-                {
-                    foreach (var childDir in Directory.GetDirectories(directory))
-                    {
-                        try
-                        {
-                            File.SetAttributes(childDir, FileAttributes.Normal);
-                        }
-                        catch (IOException)
-                        {
-                        }
-                    }
-                    Thread.Sleep(100);
-                }
-            }
         }
     }
 }
