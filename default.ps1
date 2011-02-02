342c9be8b0fb6dc50d69f22eac5f7e9ce7d631c8
fschwiet <fschwiet@gmail.com>
2011-02-01 22:40:34 -0800

    minor build change

--- default.ps1
+++ default.ps1
@@ -21,7 +21,10 @@ properties {
 task default -depends AllTests
 
 task Build {
-    rmdir $buildDir -recurse
+
+    if (test-path $buildDir) {
+        rmdir $buildDir -recurse
+    }
 
 	$v4_net_version = (ls "$env:windir\Microsoft.NET\Framework\v4.0*").Name
     exec { &"C:\Windows\Microsoft.NET\Framework\$v4_net_version\MSBuild.exe" "$sln_file" /p:OutDir="$buildDir" /property:Configuration=$msbuild_Configuration /target:Clean }
