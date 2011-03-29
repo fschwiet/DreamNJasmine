5382706466d36bdd02a000f0a9928c909f2df114
fschwiet <fschwiet@gmail.com>
2011-03-28 22:17:40 -0700

    test fix

--- NJasmine.Tests/PassingFixtures/test_name_joins_specification.cs
+++ NJasmine.Tests/PassingFixtures/test_name_joins_specification.cs
@@ -6,8 +6,8 @@ namespace NJasmineTests.PassingFixtures
     [Explicit]
     [RunExternal(true, ExpectedStrings = new string[]
     {
-        @"NJasmineTests.FailingFixtures.test_name_joins_specification, simple test",
-        @"NJasmineTests.FailingFixtures.test_name_joins_specification, simple describe, simple test",
+        @"NJasmineTests.PassingFixtures.test_name_joins_specification, simple test",
+        @"NJasmineTests.PassingFixtures.test_name_joins_specification, simple describe, simple test",
     })]
     public class test_name_joins_specification : NJasmineFixture
     {
