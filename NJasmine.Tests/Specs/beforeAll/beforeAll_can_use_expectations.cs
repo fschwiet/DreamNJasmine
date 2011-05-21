7dbaf642d695450a887e71c592fa75d07bbe75df
fschwiet <fschwiet@gmail.com>
2011-05-21 11:28:39 -0700

    added failing test

---
+++ NJasmine.Tests/Specs/beforeAll/beforeAll_can_use_expectations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NJasmine;
+using NUnit.Framework;
+
+namespace NJasmineTests.Specs.beforeAll
+{
+    [Explicit]
+    public class beforeAll_can_use_expectations : GivenWhenThenFixture
+    {
+        public override void Specify()
+        {
+            beforeAll(delegate
+            {
+                expect(() => true);
+            });
+
+            when("using expect within beforeAll", delegate
+            {
+                beforeAll(delegate
+                {
+                    expect(() => true);
+                });
+
+                it("works", delegate
+                {
+
+                });
+            });
+        }
+    }
+}
