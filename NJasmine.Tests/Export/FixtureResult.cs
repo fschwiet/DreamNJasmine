41bd9cda70fe955c52540e5cd7c1f5d7b8a98f1b
fschwiet <fschwiet@gmail.com>
2011-07-14 01:23:56 -0700

    test code

--- NJasmine.Tests/Export/FixtureResult.cs
+++ NJasmine.Tests/Export/FixtureResult.cs
@@ -2,35 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Linq;
+using NUnit.Framework;
 
 namespace NJasmineTests.Export
 {
     public class FixtureResult
     {
-        private readonly bool _passed;
-        private readonly string _output;
+        private readonly string _name;
         private readonly string _xmlOutput;
+        private XDocument _doc;
 
-        public FixtureResult(bool passed, string output, string xmlOutput)
+        public FixtureResult(string name, string xmlOutput)
         {
-            _passed = passed;
-            _output = output;
+            _name = name;
             _xmlOutput = xmlOutput;
+            _doc = XDocument.Parse(xmlOutput);
+        }
+
+        public FixtureResult succeeds()
+        {
+            int totalCount = (int)_doc.Root.Attribute("total");
+            int errorCount = GetErrorCount();
+            int failureCount = GetFailureCount();
+
+            Assert.AreEqual(0, errorCount, _name + " had errors.");
+            Assert.AreEqual(0, failureCount, _name + " had failures.");
+            Assert.AreNotEqual(0, totalCount, _name + " didn't have any tests.");
+
+            return this;
+        }
+
+        public FixtureResult failed()
+        {
+            Assert.AreNotEqual(0, GetErrorCount() + GetFailureCount(), _name + " didn't have errors.");
+
+            return this;
         }
 
         public TestResult hasTest(string name)
         {
             // should assert the test actually exists...
-            throw new NotImplementedException();
-        }
-
-        public void succeeds()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void failed()
-        {
             throw new NotImplementedException();
         }
 
@@ -59,6 +71,16 @@ namespace NJasmineTests.Export
             /*
                 update-xml $xmlFile {                    $stackTrace = get-xml -exactlyonce ""//stack-trace"";                    assert (-not ($stackTrace -like '*NJasmine.Core*')) 'Expected NJasmine.Core stack elements to be removed';            */
             throw new NotImplementedException();
+        }
+
+        private int GetErrorCount()
+        {
+            return (int)_doc.Root.Attribute("errors");
+        }
+
+        private int GetFailureCount()
+        {
+            return (int)_doc.Root.Attribute("failures");
         }
     }
 }
---
+++ NJasmine.Tests/Export/FixtureResultTest.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NJasmine;
+using NUnit.Framework;
+
+namespace NJasmineTests.Export
+{
+    [Explicit]
+    public class FixtureResultTest : GivenWhenThenFixture
+    {
+        Type expectedAssertionType = typeof(AssertionException);
+
+        public override void Specify()
+        {
+            describe("succeeds", delegate
+            {
+                it("allows a passing test result", delegate
+                {
+                    new FixtureResult("hello", GetSampleXmlResult(1)).succeeds();
+                });
+
+                var cases = new Dictionary<string, TestDelegate>();
+                
+                cases.Add("running against error", delegate
+                {
+                    var result = new FixtureResult("hello", GetSampleXmlResult(1, 1));
+                    result.succeeds();
+                });
+
+                cases.Add("running against failure", delegate
+                {
+                    var result = new FixtureResult("hello", GetSampleXmlResult(1, 0, 1));
+                    result.succeeds();
+                });
+
+                cases.Add("running against no tests", delegate
+                {
+                    var result = new FixtureResult("hello", GetSampleXmlResult(0));
+                    result.succeeds();
+                });
+
+                CheckScenariosCauseErrorWithMessageContaining(cases, "hello");
+            });
+
+            describe("failed", delegate
+            {
+                it("allows test results with errors or failures", delegate
+                {
+                    new FixtureResult("hello", GetSampleXmlResult(1, 1)).failed();
+                    new FixtureResult("hello", GetSampleXmlResult(1, 0, 1)).failed();
+                });
+
+                var cases = new Dictionary<string, TestDelegate>();
+
+                cases.Add("running against no tests", delegate
+                {
+                    var result = new FixtureResult("hello", GetSampleXmlResult(0));
+                    result.failed();
+                });
+
+                CheckScenariosCauseErrorWithMessageContaining(cases, "hello");
+            });
+        }
+
+        private void CheckScenariosCauseErrorWithMessageContaining(Dictionary<string, TestDelegate> cases, string expected)
+        {
+            foreach (var scenario in cases)
+            {
+                it("asserts when " + scenario.Key, delegate
+                {
+                    var exception = Assert.Throws(expectedAssertionType, scenario.Value);
+
+                    Assert.That(exception.Message, Is.StringContaining(expected));
+                });
+            }
+        }
+
+        static string GetSampleXmlResult(int totalCount = 0, int errorCount = 0, int failureCount = 0)
+        {
+            var result =
+                @"<?xml version=""1.0"" encoding=""utf-8"" standalone=""no""?>
+<!--This file represents the results of running a test suite-->
+<test-results name=""C:\src\NJasmine\build\NJasmine.tests.dll"" total=""$totalCount"" errors=""$errorCount"" failures=""$failureCount"" not-run=""2"" inconclusive=""0"" ignored=""0"" skipped=""0"" invalid=""2"" date=""2011-07-13"" time=""21:33:22"">
+  <environment nunit-version=""2.5.9.10348"" clr-version=""2.0.50727.5446"" os-version=""Microsoft Windows NT 6.1.7601 Service Pack 1"" platform=""Win32NT"" cwd=""C:\src\NJasmine"" machine-name=""NZNZNZ6"" user=""user"" user-domain=""nznznz6"" />
+  <culture-info current-culture=""en-US"" current-uiculture=""en-US"" />
+  <test-suite type=""Assembly"" name=""C:\src\NJasmine\build\NJasmine.tests.dll"" executed=""True"" result=""Success"" success=""True"" time=""0.805"" asserts=""0"">
+    <results>
+      <test-suite type=""Namespace"" name=""NJasmineTests"" executed=""True"" result=""Success"" success=""True"" time=""0.783"" asserts=""0"">
+        <results>
+          <test-suite type=""Namespace"" name=""Core"" executed=""True"" result=""Success"" success=""True"" time=""0.491"" asserts=""0"">
+            <results>
+              <test-suite type=""TestFixture"" name=""build_and_run_suite_with_loops"" executed=""True"" result=""Success"" success=""True"" time=""0.271"" asserts=""0"">
+                <results>
+                  <test-case name=""NJasmineTests.Core.build_and_run_suite_with_loops.can_load_tests"" executed=""True"" result=""Success"" success=""True"" time=""0.222"" asserts=""0"" />
+                  <test-case name=""NJasmineTests.Core.build_and_run_suite_with_loops.can_run_tests_a1"" executed=""True"" result=""Success"" success=""True"" time=""0.019"" asserts=""1"" />
+                  <test-case name=""NJasmineTests.Core.build_and_run_suite_with_loops.can_run_tests_a3"" executed=""True"" result=""Success"" success=""True"" time=""0.001"" asserts=""1"" />
+                </results>
+              </test-suite>
+            </results>
+          </test-suite>
+        </results>
+      </test-suite>
+    </results>
+  </test-suite>
+</test-results>
+ ";
+            result = result
+                .Replace("$errorCount", errorCount.ToString())
+                .Replace("$failureCount", failureCount.ToString())
+                .Replace("$totalCount", totalCount.ToString());
+
+            return result;
+        }
+    }
+}
