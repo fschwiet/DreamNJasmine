aeaae3cf3d3b2605fb0c3f55448731fc6ffa4ac7
fschwiet <fschwiet@gmail.com>
2011-07-14 00:03:52 -0700

    separating file

---
+++ NJasmine.Tests/Export/SuiteResult.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace NJasmineTests.Export
+{
+    public class SuiteResult
+    {
+        public SuiteResult thatsInconclusive()
+        {
+            /*
+                for-xml -exactlyOnce ""//test-suite[@name='given an outer block']"" {
+                    Assert ((get-xml ""@result"") -eq 'Inconclusive') 'Expected first outer block to be inconclusive';
+
+                    for-xml ""./results"" {
+                        Assert $false 'Expected first outer block to not have any subresults'
+                    }
+                }
+                */
+
+            throw new NotImplementedException();
+        }
+
+        public SuiteResult thatHasNoResults()
+        {
+            /*
+                for-xml -exactlyOnce ""//test-suite[@name='given an outer block']"" {
+                    Assert ((get-xml ""@result"") -eq 'Inconclusive') 'Expected first outer block to be inconclusive';
+
+                    for-xml ""./results"" {
+                        Assert $false 'Expected first outer block to not have any subresults'
+                    }
+                }
+                */
+
+            throw new NotImplementedException();
+        }
+
+        public SuiteResult thatSucceeds()
+        {
+            /*
+                for-xml -exactlyOnce ""//test-suite[@name='when ignore is set after a test']"" {
+                    Assert ((get-xml ""@result"") -eq 'Success') 'Expected when statement with non-ignored test to have succeeded.';
+
+                    for-xml -exactlyOnce ""./results/test-case"" {
+                        Assert ((get-xml ""@name"") -eq 'NJasmineTests.Specs.can_mark_tests_as_ignored, when ignore is set after a test, then the earlier test runs') `
+                            'Expected when statement with non-ignored test to contain the non-ignored test'
+
+                        Assert ((get-xml ""@result"") -eq 'Success') 'Expected non-ignored test to have passed';
+                    }
+                }
+                */
+
+            throw new NotImplementedException();
+        }
+
+        public SuiteResult thatHasOneResult()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TestResult ShouldHaveTest(string testName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void withCategories(params string[] categories)
+        {
+            /*
+                                 function assertShouldHaveCategories($typeName, $name, $expectedCategories) {
+
+                    $nonempty = $expectedCategories.length -gt 0
+
+                    for-xml -exactlyOnce:$nonempty ""//$typeName[@name='$name']/categories"" {
+
+                        foreach($expectedCategory in $expectedCategories) {
+                            Assert ((get-xml ""category[@name='$expectedCategory']"") -ne  $null) `
+                                ""Expected '$test' to have category $expectedCategory""
+                        }
+
+                        for-xml ""category"" {
+
+                            $otherCategoryName = get-xml ""@name""
+                            Assert ($expectedCategories -contains $otherCategoryName) ""Expected '$test' to NOT have category $otherCategoryName""
+                        }
+                    }
+                }
+
+                assertShouldHaveCategories 'test-suite' 'when using category Foo then Bar' @()
+
+                assertShouldHaveCategories 'test-case' 'NJasmineTests.Specs.supports_categories, when using category Foo then Bar, then tests have Foo' @(""Foo"")* 
+                 */
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NJasmine.Tests/Export/TestResult.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace NJasmineTests.Export
+{
+    public class TestResult
+    {
+        public TestResult withMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TestResult thatSucceeds()
+        {
+            // $null = get-xml -exactlyOnce ""//test-case[@name='NJasmineTests.Specs.beforeAll.beforeAll_can_use_expectations, works'][@result='Success']"" 
+            throw new NotImplementedException();
+        }
+
+        public TestResult thatErrors()
+        {
+            // $null = get-xml -exactlyOnce ""//test-case[@name='NJasmineTests.Specs.beforeAll.beforeAll_can_use_expectations, when using expect within beforeAll, fails'][@result='Error']"" 
+            throw new NotImplementedException();
+        }
+
+        public TestResult thatFailsInAnUnspecifiedManner()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TestResult thatIsNotRunnable()
+        {
+            // console output:  "NotRunnable : NJasmineTests.Specs.duplicate_test_names_are_fine, repeated unimplemented outer test",
+            throw new NotImplementedException();
+        }
+
+        public TestResult thatFails()
+        {
+            // console output: "Test Failure : NJasmineTests.Specs.duplicate_test_names_are_fine, repeated describe, repeated inner describe",
+            throw new NotImplementedException();
+        }
+
+        public TestResult withCategories(params string[] categories)
+        {
+            /*
+                    update-xml $xmlFile {
+
+                        function assertShouldHaveCategories($typeName, $name, $expectedCategories) {
+
+                            $nonempty = $expectedCategories.length -gt 0
+
+                            for-xml -exactlyOnce:$nonempty ""//$typeName[@name='$name']/categories"" {
+
+                                foreach($expectedCategory in $expectedCategories) {
+                                    Assert ((get-xml ""category[@name='$expectedCategory']"") -ne  $null) `
+                                        ""Expected '$test' to have category $expectedCategory""
+                                }
+
+                                for-xml ""category"" {
+
+                                    $otherCategoryName = get-xml ""@name""
+                                    Assert ($expectedCategories -contains $otherCategoryName) ""Expected '$test' to NOT have category $otherCategoryName""
+                                }
+                            }
+                        }
+
+                        assertShouldHaveCategories 'test-suite' 'when using category Foo then Bar' @()
+
+                        assertShouldHaveCategories 'test-case' 'NJasmineTests.Specs.supports_categories, when using category Foo then Bar, then tests have Foo' @(""Foo"")                 */
+
+            throw new NotImplementedException();
+        }
+    }
+}
