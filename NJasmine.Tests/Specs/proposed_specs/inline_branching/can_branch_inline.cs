94c1defc6864e257ef9dd31f29079857421b288b
fschwiet <fschwiet@gmail.com>
2011-09-16 15:11:27 -0700

    proposed feature: inline branching

---
+++ NJasmine.Tests/Specs/proposed_specs/inline_branching/can_branch_inline.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NJasmine;
+using NJasmineTests.Export;
+
+namespace NJasmineTests.Specs.proposed_specs.inline_branching
+{
+    class can_branch_inline : GivenWithThenFixtureWithInlineBranching, INJasmineInternalRequirement
+    {
+        public override void Specify()
+        {
+            given("some precondition", delegate
+            {
+                int input = -1;
+                string expectedResult = null;
+
+                branch(
+                    finish => when("the input is 0", delegate
+                    {
+                        input = 0;
+                        expectedResult = input.ToString();
+
+                        // do special setup
+
+                        finish();
+                    }),
+                    finish => when("the input is 1", delegate
+                    {
+                        input = 1;
+                        expectedResult = input.ToString();
+
+                        // other special setup
+
+                        finish();
+                    }));
+
+                then("it runs" + input, delegate
+                {
+                    expect(() => input.ToString() == expectedResult);
+                });
+            });
+        }
+
+        public void Verify_NJasmine_implementation(FixtureResult fixtureResult)
+        {
+            fixtureResult.hasTest("given some precondition, when the input is 0, then it runs").thatSucceeds();
+            fixtureResult.hasTest("given some precondition, when the input is 1, then it runs").thatSucceeds();
+        }
+    }
+
+    public abstract class GivenWithThenFixtureWithInlineBranching : GivenWhenThenFixture
+    {
+        protected void branch(params Action<Action>[] action)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
