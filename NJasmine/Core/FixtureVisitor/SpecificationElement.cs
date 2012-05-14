b6ad3322b7aaa62d882e388b56b1311afb2c48ea
fschwiet <fschwiet@gmail.com>
2012-05-13 22:57:53 -0700

    replacing enum SpecElement with this class

---
+++ NJasmine/Core/FixtureVisitor/SpecificationElement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NJasmine.Core.FixtureVisitor
+{
+    public class SpecificationElement
+    {
+        readonly SpecElement _element;
+
+        public SpecificationElement(SpecElement element)
+        {
+            _element = element;
+        }
+
+        public SpecElement Value { 
+            get { return _element; } 
+        }
+    }
+}
