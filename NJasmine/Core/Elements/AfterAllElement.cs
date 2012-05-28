708b82881e28b604bcf38604a6bca3ec12cd44d5
fschwiet <fschwiet@gmail.com>
2012-05-28 14:19:24 -0700

    moar elements

---
+++ NJasmine/Core/Elements/AfterAllElement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NJasmine.Core.FixtureVisitor;
+
+namespace NJasmine.Core.Elements
+{
+    public class AfterAllElement : SpecificationElement
+    {
+        readonly Action _action;
+
+        public AfterAllElement(ActualKeyword actualKeyword, Action action)
+            : base(actualKeyword)
+        {
+            _action = action;
+        }
+
+        public override object Run(ISpecPositionVisitor visitor, TestPosition position)
+        {
+            visitor.visitAfterAll(this, _action, position);
+            return ElementResultUnused;
+        }
+    }
+}
---
+++ NJasmine/Core/Elements/ExpectElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using NJasmine.Core.FixtureVisitor;
+
+namespace NJasmine.Core.Elements
+{
+    public class ExpectElement : SpecificationElement
+    {
+        readonly Expression<Func<bool>> _expectation;
+
+        public ExpectElement(ActualKeyword actualKeyword, Expression<Func<bool>> expectation)
+            : base(actualKeyword)
+        {
+            _expectation = expectation;
+        }
+
+        public override object Run(ISpecPositionVisitor visitor, TestPosition position)
+        {
+            visitor.visitExpect(this, _expectation, position);
+            return ElementResultUnused;
+        }
+    }
+}
