64e4a7f73215a45a3d35883c9069508166edaf16
fschwiet <fschwiet@gmail.com>
2012-06-03 16:42:17 -0700

    proposed spec: NJasmine on Razor

---
+++ NJasmine.Tests/Specs/proposed_specs/RazorSpecs/RazorFixture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NJasmine;
+
+namespace NJasmineTests.Specs.proposed_specs.RazorSpecs
+{
+    public abstract class RazorFixture : GivenWhenThenFixture
+    {
+        public override void Specify()
+        {
+            throw new NotImplementedException();
+        }
+
+        public new string describe(string description, Action action)
+        {
+            base.describe(description, action);
+            return "";
+        }
+
+        public new string it(string description, Action action)
+        {
+            base.it(description, action);
+            return "";
+        }
+    }
+}
---
+++ NJasmine.Tests/Specs/proposed_specs/RazorSpecs/Romanizer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NJasmineTests.Specs.proposed_specs.RazorSpecs
+{
+    public class Romanizer
+    {
+        public static string Get(int key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
