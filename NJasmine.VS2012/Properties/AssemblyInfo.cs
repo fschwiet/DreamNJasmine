ae0f70dcc08d6628d8d71c90a61dfca316ae94c2
fschwiet <fschwiet@gmail.com>
2012-07-15 01:39:49 -0700

    fix new project type to be C#

--- NJasmine.VS2012/Properties/AssemblyInfo.cs
+++ NJasmine.VS2012/Properties/AssemblyInfo.cs
@@ -1,36 +1,20 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("NJasmine.VS2012")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NJasmine.VS2012")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+#if !SILVERLIGHT
+[assembly: SuppressIldasmAttribute()]
+#endif
+[assembly: CLSCompliantAttribute(false )]
+[assembly: ComVisibleAttribute(false)]
+[assembly: AssemblyTitleAttribute("NJasmine.VS2012 0.3.0.0")]
+[assembly: AssemblyDescriptionAttribute("NJasmine allows writing RSpec-like specifications.")]
+[assembly: AssemblyCompanyAttribute("n/a")]
+[assembly: AssemblyProductAttribute("NJasmine 0.3.0.0")]
+[assembly: AssemblyCopyrightAttribute("Copyright © Frank Schwieterman 2010-2011")]
+[assembly: AssemblyVersionAttribute("0.3.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("0.3.0.0 / 0000000")]
+[assembly: AssemblyFileVersionAttribute("0.3.0.0")]
+[assembly: AssemblyDelaySignAttribute(false)]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("09fee721-c2c3-4310-a145-52c088474211")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
