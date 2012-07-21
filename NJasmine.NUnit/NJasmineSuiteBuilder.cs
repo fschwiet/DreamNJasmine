84a12d62c9b28bc7dfb56f497df44be7b4e1d9f6
fschwiet <fschwiet@gmail.com>
2012-07-21 00:40:27 -0700

    move more out of NJasmine.NUnit

--- NJasmine.NUnit/NJasmineSuiteBuilder.cs
+++ NJasmine.NUnit/NJasmineSuiteBuilder.cs
@@ -29,38 +29,7 @@ namespace NJasmine.NUnit
         {
             NativeTestFactory nativeTestFactory = new NativeTestFactory();
 
-            var constructor = type.GetConstructor(new Type[0]);
-
-            Func<SpecificationFixture> fixtureFactory = delegate()
-            {
-                var fixture = constructor.Invoke(new object[0]) as SpecificationFixture;
-                return fixture;
-            };
-
-            SharedContext buildContext = new SharedContext(nativeTestFactory, fixtureFactory, new NameReservations(), fixtureFactory());
-
-            var testContext = new TestContext()
-            {
-                Position = new TestPosition(),
-                GlobalSetupManager = buildContext.GetGlobalSetupManager(),
-                Name = new TestName
-                {
-                    FullName = type.Namespace + "." + type.Name,
-                    Shortname = type.Name,
-                    MultilineName = type.Namespace + "." + type.Name
-                }
-            };
-
-            TestSuite rootSuite = new TestSuite(buildContext, testContext);
-
-            TestName name = new TestName
-            {
-                FullName = type.Namespace + "." + type.Name,
-                Shortname = type.Name,
-                MultilineName = type.Namespace + "." + type.Name
-            };
-
-            TestBuilder root = rootSuite.RunSuiteAction(buildContext.GetSpecificationRootAction(), true, new TestBuilder(buildContext.NativeTestFactory.ForSuite(testContext, () => testContext.GlobalSetupManager.Cleanup(testContext.Position)), testContext.Name));
+            var root = SpecificationBuilder.BuildTestFixture(type, nativeTestFactory);
 
             var result = (root.GetUnderlyingTest() as NativeTest).GetNative(root);
 
---
+++ NJasmine/Core/SpecificationBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NJasmine.Core.Discovery;
+
+namespace NJasmine.Core
+{
+    public class SpecificationBuilder
+    {
+        public static TestBuilder BuildTestFixture(Type type, INativeTestFactory nativeTestFactory)
+        {
+            var constructor = type.GetConstructor(new Type[0]);
+
+            Func<SpecificationFixture> fixtureFactory = delegate()
+            {
+                var fixture = constructor.Invoke(new object[0]) as SpecificationFixture;
+                return fixture;
+            };
+
+            SharedContext buildContext = new SharedContext(nativeTestFactory, fixtureFactory, new NameReservations(),
+                                                           fixtureFactory());
+
+            var testContext = new TestContext()
+            {
+                Position = new TestPosition(),
+                GlobalSetupManager = buildContext.GetGlobalSetupManager(),
+                Name = new TestName
+                {
+                    FullName = type.Namespace + "." + type.Name,
+                    Shortname = type.Name,
+                    MultilineName = type.Namespace + "." + type.Name
+                }
+            };
+
+            TestSuite rootSuite = new TestSuite(buildContext, testContext);
+
+            var resultBuilder = new TestBuilder(buildContext.NativeTestFactory.ForSuite(testContext, () => testContext.GlobalSetupManager.Cleanup(testContext.Position)), testContext.Name);
+
+            return rootSuite.RunSuiteAction(buildContext.GetSpecificationRootAction(), true, resultBuilder);
+        }
+    }
+}
