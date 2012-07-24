c8cc7b02f229b6f5169cc78e01acc954f6d5936c
fschwiet <fschwiet@gmail.com>
2012-07-24 00:12:42 -0700

    add spec

---
+++ NJasmine.Tests/Specs/expectations/should_ignore_expectations_after_test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NJasmine;
+using NJasmineTests.Export;
+
+namespace NJasmineTests.Specs.expectations
+{
+    public class should_ignore_expectations_after_test : GivenWhenThenFixture, INJasmineInternalRequirement
+    {
+        public override void Specify()
+        {
+            it("has a passing test", () => {});
+
+            expect(() => false);
+            expectEventually(() => false);
+
+            it("has a failing test", () => {});
+        }
+
+        public void Verify_NJasmine_implementation(FixtureResult fixtureResult)
+        {
+            fixtureResult.hasTest("has a passing test").thatSucceeds();
+            fixtureResult.hasTest("has a failing test").thatErrors();
+        }
+    }
+}
