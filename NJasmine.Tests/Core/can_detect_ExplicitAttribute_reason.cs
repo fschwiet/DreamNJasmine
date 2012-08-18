136a20d29e906c2502aa1f8371586e2364dc5500
fschwiet <fschwiet@gmail.com>
2012-08-18 16:35:51 -0700

    have code to check ExplicitAttribute without NUnit.Framework reference

---
+++ NJasmine.Tests/Core/can_detect_ExplicitAttribute_reason.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NJasmine;
+using NJasmine.Core;
+using NUnit.Framework;
+
+namespace NJasmineTests.Core
+{
+    public class can_detect_ExplicitAttribute_reason : GivenWhenThenFixture
+    {
+        class ClassWithoutExplicitAttribute
+        {
+        }
+
+        [Explicit]
+        class ClassWithExplicitAttribute
+        {
+        }
+
+        [Explicit("actually was dolan")]
+        class ClassWithExplicitAttributeReason
+        {
+        }
+
+        public override void Specify()
+        {
+            it("can detect a class does not have ExplicitAttribute", () =>
+            {
+                expect(() => ExplicitAttributeReader.GetFor(typeof (ClassWithoutExplicitAttribute)) == null);
+            });
+
+            it("can detect a class does have Explicit Attribute", () =>
+            {
+                expect(() => ExplicitAttributeReader.GetFor(typeof(ClassWithExplicitAttribute)) == "ClassWithoutExplicitAttribute has attribute Explicit.");
+            });
+
+            it("can detect the reason attribute of ExplicitAttribute when available", () =>
+            {
+                expect(() => ExplicitAttributeReader.GetFor(typeof(ClassWithExplicitAttributeReason)) == "actually was dolan");
+            });
+        }
+    }
+}
---
+++ NJasmine/Core/ExplicitAttributeReader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace NJasmine.Core
+{
+    public class ExplicitAttributeReader
+    {
+        public static string GetFor(Type type)
+        {
+            var explicitAttributes = type.GetCustomAttributes(false)
+                .Where(a => a.GetType().FullName.Contains("NUnit") && a.GetType().Name == "ExplicitAttribute");
+
+            if (explicitAttributes.Count() == 0)
+                return null;
+
+            string explicitReason = null;
+            foreach (var explicitAttribute in explicitAttributes)
+            {
+                var reasonProperty = explicitAttribute.GetType().GetProperty("Reason",
+                    BindingFlags.Public | BindingFlags.Instance);
+
+                if (reasonProperty != null)
+                {
+                    var reason = (string)reasonProperty.GetGetMethod(false).Invoke(explicitAttribute, new object[0]);
+
+                    if (!string.IsNullOrEmpty(reason))
+                    {
+                        explicitReason = reason;
+                    }
+                }
+            }
+
+            if (explicitReason == null)
+                return "ClassWithoutExplicitAttribute has attribute Explicit.";
+            else
+                return explicitReason;
+        }
+    }
+}
