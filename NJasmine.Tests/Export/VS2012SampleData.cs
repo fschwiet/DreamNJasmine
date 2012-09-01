91ec1bbb6e913d450ad40d5e00a879a4f78a01f7
fschwiet <fschwiet@gmail.com>
2012-09-01 11:36:31 -0700

    updating VS2012 sample data

--- NJasmine.Tests/Export/VS2012SampleData.cs
+++ NJasmine.Tests/Export/VS2012SampleData.cs
@@ -26,62 +26,45 @@ at NJasmine.GivenWhenThenFixture.<>c__DisplayClass13.<beforeAll>b__11() in c:\sr
 ")
         {
             var result = @"<?xml version='1.0' encoding='UTF-8'?>
-<TestRun id='3b3f1e83-e5b2-4d21-9e60-32459ef35961' name='user@NZNZNZ6 2012-09-01 10:44:22' runUser='nznznz6\user' xmlns='http://microsoft.com/schemas/VisualStudio/TeamTest/2010'>
-  <Times creation='2012-09-01T10:44:22.3933642-07:00' queuing='2012-09-01T10:44:22.3943642-07:00' start='2012-09-01T10:44:22.3993645-07:00' finish='2012-09-01T10:44:22.4613680-07:00' />
-  <TestSettings name='default' id='45bfd059-4ef9-4cc0-8e6b-20574a536812'>
+<TestRun id='6845f341-9fa0-418c-b166-3746876b23e4' name='user@NZNZNZ6 2012-09-01 11:31:08' runUser='nznznz6\user' xmlns='http://microsoft.com/schemas/VisualStudio/TeamTest/2010'>
+  <Times creation='2012-09-01T11:31:08.2730406-07:00' queuing='2012-09-01T11:31:08.2740407-07:00' start='2012-09-01T11:31:08.2780409-07:00' finish='2012-09-01T11:31:08.3280438-07:00' />
+  <TestSettings name='default' id='989b80fc-378d-41c9-a7f3-a4d52214ebc7'>
     <Execution>
       <TestTypeSpecific />
     </Execution>
-    <Deployment runDeploymentRoot='user_NZNZNZ6 2012-09-01 10_44_22' />
+    <Deployment runDeploymentRoot='user_NZNZNZ6 2012-09-01 11_31_08' />
   </TestSettings>
   <Results>
-    <UnitTestResult executionId='2d21219e-f85d-4792-98f9-492b7f35f39f' testId='82fe35ec-feca-4d3c-9fed-43013c8f7508' testName='NamespaceIsntNJasmineTests.stacktrace_has_NJasmine_internal_calls_removed, given some context, when some action, then it fails' computerName='NZNZNZ6' startTime='2012-09-01T10:44:22.4453671-07:00' endTime='2012-09-01T10:44:22.4453671-07:00' testType='13cdc9d9-ddb5-4fa4-a97d-d965ccfc6d4b' outcome='Failed' testListId='8c84fa94-04c1-424b-9868-57a2d4851a1d' relativeResultsDirectory='2d21219e-f85d-4792-98f9-492b7f35f39f'>
+    <UnitTestResult executionId='41878d87-8115-4db2-bf21-1179dfac85fe' testId='668cee77-1f18-495f-8601-8133039e3755' testName='NamespaceIsntNJasmineTests.stacktrace_has_NJasmine_internal_calls_removed, given some context, when some action, then it fails' computerName='NZNZNZ6' startTime='2012-09-01T11:31:08.3140430-07:00' endTime='2012-09-01T11:31:08.3140430-07:00' testType='13cdc9d9-ddb5-4fa4-a97d-d965ccfc6d4b' outcome='Failed' testListId='8c84fa94-04c1-424b-9868-57a2d4851a1d' relativeResultsDirectory='41878d87-8115-4db2-bf21-1179dfac85fe'>
       <Output>
         <ErrorInfo>
           <Message>System.Exception : IsTrue failed, expression was:
 
 False</Message>
-          <StackTrace>SPECIFICATION:
-    NamespaceIsntNJasmineTests.stacktrace_has_NJasmine_internal_calls_removed,
-    given some context,
-    when some action,
-    then it fails
-
-Using odd namespace so callstack is filtered.
-
-at PowerAssert.PAssert.IsTrue(Expression`1 expression)
-at Expect.cs:11  NJasmine.Extras.Expect.That(Expression`1 expectation) in c:\src\NJasmine\NJasmine\Extras\Expect.cs:line 11
-at GivenWhenThenFixture.cs:186  NJasmine.GivenWhenThenFixture.expect(Expression`1 expectation) in c:\src\NJasmine\NJasmine\GivenWhenThenFixture.cs:line 186
-at stacktrace_has_NJasmine_internal_calls_removed.cs:24  NamespaceIsntNJasmineTests.stacktrace_has_NJasmine_internal_calls_removed.&lt;Specify&gt;b__2() in c:\src\NJasmine\NJasmine.Tests\Specs\report_test_failures_usefully\stacktrace_has_NJasmine_internal_calls_removed.cs:line 24
-at GivenWhenThenFixture.cs:50  NJasmine.GivenWhenThenFixture.then(String description, Action test) in c:\src\NJasmine\NJasmine\GivenWhenThenFixture.cs:line 50
-at stacktrace_has_NJasmine_internal_calls_removed.cs:22  NamespaceIsntNJasmineTests.stacktrace_has_NJasmine_internal_calls_removed.&lt;Specify&gt;b__1() in c:\src\NJasmine\NJasmine.Tests\Specs\report_test_failures_usefully\stacktrace_has_NJasmine_internal_calls_removed.cs:line 22
-at GivenWhenThenFixture.cs:40  NJasmine.GivenWhenThenFixture.when(String description, Action specification) in c:\src\NJasmine\NJasmine\GivenWhenThenFixture.cs:line 40
-at stacktrace_has_NJasmine_internal_calls_removed.cs:20  NamespaceIsntNJasmineTests.stacktrace_has_NJasmine_internal_calls_removed.&lt;Specify&gt;b__0() in c:\src\NJasmine\NJasmine.Tests\Specs\report_test_failures_usefully\stacktrace_has_NJasmine_internal_calls_removed.cs:line 20
-at GivenWhenThenFixture.cs:30  NJasmine.GivenWhenThenFixture.given(String description, Action specification) in c:\src\NJasmine\NJasmine\GivenWhenThenFixture.cs:line 30
-at stacktrace_has_NJasmine_internal_calls_removed.cs:18  NamespaceIsntNJasmineTests.stacktrace_has_NJasmine_internal_calls_removed.Specify() in c:\src\NJasmine\NJasmine.Tests\Specs\report_test_failures_usefully\stacktrace_has_NJasmine_internal_calls_removed.cs:line 18
-
+          <StackTrace>$aStackTrace
 </StackTrace>
         </ErrorInfo>
       </Output>
     </UnitTestResult>
   </Results>
   <TestDefinitions>
-    <UnitTest name='NamespaceIsntNJasmineTests.stacktrace_has_NJasmine_internal_calls_removed, given some context, when some action, then it fails' storage='c:\src\njasmine\bin\njasmine.tests.dll' id='82fe35ec-feca-4d3c-9fed-43013c8f7508'>
-      <Execution id='2d21219e-f85d-4792-98f9-492b7f35f39f' />
+    <UnitTest name='NamespaceIsntNJasmineTests.stacktrace_has_NJasmine_internal_calls_removed, given some context, when some action, then it fails' storage='c:\src\njasmine\bin\njasmine.tests.dll' id='668cee77-1f18-495f-8601-8133039e3755'>
+      <Execution id='41878d87-8115-4db2-bf21-1179dfac85fe' />
       <TestMethod codeBase='C:\src\NJasmine\bin\NJasmine.Tests.dll' adapterTypeName='Microsoft.VisualStudio.TestTools.TestTypes.Unit.UnitTestAdapter' className='NamespaceIsntNJasmineTests' name='NamespaceIsntNJasmineTests.stacktrace_has_NJasmine_internal_calls_removed, given some context, when some action, then it fails' />
     </UnitTest>
   </TestDefinitions>
   <TestEntries>
-    <TestEntry testId='82fe35ec-feca-4d3c-9fed-43013c8f7508' executionId='2d21219e-f85d-4792-98f9-492b7f35f39f' testListId='8c84fa94-04c1-424b-9868-57a2d4851a1d' />
+    <TestEntry testId='668cee77-1f18-495f-8601-8133039e3755' executionId='41878d87-8115-4db2-bf21-1179dfac85fe' testListId='8c84fa94-04c1-424b-9868-57a2d4851a1d' />
   </TestEntries>
   <TestLists>
     <TestList name='Results Not in a List' id='8c84fa94-04c1-424b-9868-57a2d4851a1d' />
     <TestList name='All Loaded Results' id='19431567-8539-422a-85d7-44ee4e166bda' />
   </TestLists>
   <ResultSummary outcome='Failed'>
-    <Counters total='1' executed='1' passed='0' failed='1' error='0' timeout='0' aborted='0' inconclusive='0' />
+    <Counters total='$totalCount' executed='1' passed='0' failed='$failureCount' error='$errorCount' timeout='0' aborted='0' inconclusive='0' passedButRunAborted='0' notRunnable='0' notExecuted='0' disconnected='0' warning='0' completed='0' inProgress='0' pending='0' />
   </ResultSummary>
-</TestRun>";
+</TestRun>
+";
             result = result
                 .Replace("$errorCount", errorCount.ToString())
                 .Replace("$failureCount", failureCount.ToString())
